685dbbf5942cdf0196d967e4b262363712bc7c47
Georgy Angelov <georgyangelov@gmail.com>
2013-04-27 18:35:06 +0300

    Add @delay decorator to delay function call and mask multiple frequent calls

---
+++ libs/decorator_utils.py
@@ -0,0 +1,32 @@
+from threading import Timer
+
+
+def delay(time, key_func):
+    """ Delay given function so that calls
+    in short intervals result in one call.
+
+    Only calls with arguments that are the same for key_func(*args, **kwargs)
+    are masked and delayed.
+    """
+    delayed_calls = dict()
+
+    def decorator(func):
+        def decorated_func(self, *args, **kwargs):
+            key = key_func(*args, **kwargs)
+
+            def callback():
+                del delayed_calls[key]
+
+                # bind `func` to `self`
+                bound_func = func.__get__(self, self.__class__)
+                bound_func(*args, **kwargs)
+
+            if key in delayed_calls and delayed_calls[key].is_alive():
+                delayed_calls[key].cancel()
+
+            delayed_calls[key] = Timer(time, callback)
+            delayed_calls[key].start()
+
+        return decorated_func
+
+    return decorator
