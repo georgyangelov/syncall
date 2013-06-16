67651242adefd390b84c3a6bacb69941c6dffe1b
Georgy Angelov <georgyangelov@gmail.com>
2013-06-16 22:05:43 +0300

    Finally a way to detect which files are changed based on the index

--- syncall/Directory.py
+++ syncall/Directory.py
@@ -13,6 +13,11 @@ class Directory:
     Listens for file system changes in specific directory and applies
     changes from different sources.
     """
+
+    CONFLICT = -1
+    NOT_MODIFIED = 0
+    NEEDS_UPDATE = 1
+
     def __init__(self, uuid, dir_path, index_name='.syncall_index'):
         self.logger = logging.getLogger(__name__)
 
@@ -99,3 +104,61 @@ class Directory:
 
         if 'deleted' in file_data:
             del file_data['deleted']
+
+    def get_changes(self, remote_index):
+        """
+        Return (modified_files, deleted_files, conflicted_files) where
+        modified_files, deleted_files and conflicted_files are sets of file
+        names of files that need to be changed on the remote store.
+
+        Files that need to be transferred from the remote store to the
+        current one are not detected as it's handled on the remote side.
+
+        A full two-way synchronization is handled as two separate
+        one-way synchronizations (local -> remote and remote -> local).
+
+        TODO: Support deleted files detection
+        """
+        modified_files = set()
+        deleted_files = set()
+        conflicted_files = set()
+
+        for (file, file_data) in self._index:
+            sync_status = Directory._compare_file(
+                file_data,
+                remote_index.get(file, None)
+            )
+
+            if sync_status == NEEDS_UPDATE:
+                modified_files.add(file)
+            elif sync_status == CONFLICT:
+                conflicted_files.add(file)
+
+        return (modified_files, deleted_files, conflicted_files)
+
+    def _compare_file(self, local, remote):
+        """
+        Compare two files by their index data. remote can be None if remote
+        data is not present.
+
+        Return NEEDS_UPDATE if file needs to be synchronized (local -> remote),
+        CONFLICT on conflict and NOT_MODIFIED otherwise.
+        """
+        if remote is None:
+            if 'deleted' not in local or not local['deleted']:
+                return NEEDS_UPDATE
+
+        if local['last_update_location'] in remote['sync_log'] and
+        local['last_update'] <=
+        remote['sync_log'][local['last_update_location']]:
+            # File on remote is either the same or derived from this one
+            return NOT_MODIFIED
+
+        elif remote['last_update_location'] in local['sync_log'] and
+        remote['last_update'] <=
+        local['sync_log'][remote['last_update_location']]:
+            # File needs to be transferred to remote
+            return NEEDS_UPDATE
+
+        # Files are in conflict
+        return CONFLICT
