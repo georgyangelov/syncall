2b7a86e5211edf1a472c65b92e54aa6be6c5ccf0
Georgy Angelov <georgyangelov@gmail.com>
2013-06-17 23:26:18 +0300

    Simple conflict detection test & refractor the diff tests a little

--- syncall_tests/index_diff.py
+++ syncall_tests/index_diff.py
@@ -5,6 +5,11 @@ import syncall
 
 class IndexDiffTests(unittest.TestCase):
 
+    def assertDiff(self, diff, updates, deletes, conflicts):
+        self.assertEqual(diff[0], updates)
+        self.assertEqual(diff[1], deletes)
+        self.assertEqual(diff[2], conflicts)
+
     def setUp(self):
         self.dirA = syncall.Directory('A', 'dummy_dir', load_index=False)
         self.dirB = syncall.Directory('B', 'dummy_dir', load_index=False)
@@ -23,16 +28,10 @@ class IndexDiffTests(unittest.TestCase):
         self.dirB._index = {}
 
         diffAB = self.dirA.diff(self.dirB._index)
-        self.assertEqual(diffAB[0], {
-            'dir/file1'
-        })
-        self.assertEqual(diffAB[1], set())
-        self.assertEqual(diffAB[2], set())
+        self.assertDiff(diffAB, {'dir/file1'}, set(), set())
 
         diffBA = self.dirB.diff(self.dirA._index)
-        self.assertEqual(diffBA[0], set())
-        self.assertEqual(diffBA[1], set())
-        self.assertEqual(diffBA[2], set())
+        self.assertDiff(diffBA, set(), set(), set())
 
     def test_modified_file_simple(self):
         self.dirA._index = {
@@ -58,13 +57,35 @@ class IndexDiffTests(unittest.TestCase):
         }
 
         diffAB = self.dirA.diff(self.dirB._index)
-        self.assertEqual(diffAB[0], set())
-        self.assertEqual(diffAB[1], set())
-        self.assertEqual(diffAB[2], set())
+        self.assertDiff(diffAB, set(), set(), set())
 
         diffBA = self.dirB.diff(self.dirA._index)
-        self.assertEqual(diffBA[0], {
-            'dir/file1'
-        })
-        self.assertEqual(diffBA[1], set())
-        self.assertEqual(diffBA[2], set())
+        self.assertDiff(diffBA, {'dir/file1'}, set(), set())
+
+    def test_conflict_simple(self):
+        self.dirA._index = {
+            'dir/file1': {
+                'last_update': 5,
+                'last_update_location': 'A',
+                'hash': 'A5',
+                'sync_log': {
+                    'A': 5
+                }
+            }
+        }
+        self.dirB._index = {
+            'dir/file1': {
+                'last_update': 6,
+                'last_update_location': 'B',
+                'hash': 'B6',
+                'sync_log': {
+                    'B': 6
+                }
+            }
+        }
+
+        diffAB = self.dirA.diff(self.dirB._index)
+        self.assertDiff(diffAB, set(), set(), {'dir/file1'})
+
+        diffBA = self.dirB.diff(self.dirA._index)
+        self.assertDiff(diffBA, set(), set(), {'dir/file1'})
