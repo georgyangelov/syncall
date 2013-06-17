617f64dd8682036452b2cf6d0a4bae65b830f909
Georgy Angelov <georgyangelov@gmail.com>
2013-06-17 23:02:31 +0300

    Rename syncall_tests/Directory.py to indexing.py to reflect what is tested

--- syncall_tests/indexing.py
+++ syncall_tests/indexing.py
@@ -6,7 +6,7 @@ import bintools
 import syncall
 
 
-class DirectoryTests(unittest.TestCase):
+class DirectoryIndexTests(unittest.TestCase):
     TEST_DIR = os.path.dirname(os.path.realpath(__file__)) + '/test_files'
 
     TEST_FILES = (
@@ -39,6 +39,12 @@ class DirectoryTests(unittest.TestCase):
             self.assertIn('last_update_location', file_data)
             self.assertEqual(file_data['last_update_location'], 'uuid')
             self.assertIn('hash', file_data)
+            self.assertIn('sync_log', file_data)
+            self.assertIn('uuid', file_data['sync_log'])
+            self.assertEqual(
+                file_data['sync_log']['uuid'],
+                file_data['last_update']
+            )
 
         self.assertTrue(os.path.exists(self.TEST_DIR + '/.syncall_index'))
         os.remove(self.TEST_DIR + '/.syncall_index')
@@ -62,6 +68,12 @@ class DirectoryTests(unittest.TestCase):
             self.assertEqual(
                 file_data['hash'],
                 bintools.hash_file(self.TEST_DIR + '/' + path)
+            )
+            self.assertIn('sync_log', file_data)
+            self.assertIn('uuid', file_data['sync_log'])
+            self.assertEqual(
+                file_data['sync_log']['uuid'],
+                file_data['last_update']
             )
 
         os.remove(self.TEST_DIR + '/.syncall_index')
@@ -88,6 +100,11 @@ class DirectoryTests(unittest.TestCase):
         # File shouldn't be detected as changed if hashes are the same
         self.assertEqual(readme_file_data['last_update'], old_last_update)
         self.assertEqual(readme_file_data['last_update_location'], 'uuid')
+        self.assertIn('uuid', readme_file_data['sync_log'])
+        self.assertEqual(
+            readme_file_data['sync_log']['uuid'],
+            readme_file_data['last_update']
+        )
 
         os.remove(self.TEST_DIR + '/.syncall_index')
 
@@ -119,6 +136,11 @@ class DirectoryTests(unittest.TestCase):
 
         self.assertEqual(readme_file_data['last_update_location'], 'uuid_new')
         self.assertGreater(readme_file_data['last_update'], old_last_update)
+        self.assertIn('uuid_new', readme_file_data['sync_log'])
+        self.assertEqual(
+            readme_file_data['sync_log']['uuid_new'],
+            readme_file_data['last_update']
+        )
 
         self.assertIn('animals/added_file.txt', directory._index)
 
