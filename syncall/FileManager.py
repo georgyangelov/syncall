87b0256e4851db417b9f6465c3e5489ec1b63402
Georgy Angelov <georgyangelov@gmail.com>
2013-06-18 22:26:58 +0300

    Synchronize file sync logic & add some placeholders

--- syncall/FileManager.py
+++ syncall/FileManager.py
@@ -5,22 +5,64 @@ from threading import Thread
 
 class FileManager:
     def __init__(self, remote):
+        self.logger = logging.getLogger(__name__)
+
         self.remote = remote
         self.directory = remote.directory
 
-        self.logger = logging.getLogger(__name__)
+        self.transfers_lock = threading.Lock()
 
-    def sync_file(self, file):
-        self.logger.debug("Syncing {}".format(file))
+        self.transfers = dict()
+
+    def sync_file(self, file, lock=True):
+        with self.transfers_lock:
+            if file in self.transfers:
+                transfer = self.transfers[file]
+
+                if transfer.file_data != self.directory.get_index()[file]:
+                    transfer.shutdown()
+                    del self.transfers[file]
+
+            self.logger.debug("Syncing {}".format(file))
+
+            # File is guaranteed to be indexed locally
+            # so the `file` key exists
+            transfer = FileTransfer(self.directory.get_index()[file])
+            self.transfers[file] = transfer
+
+            transfer.start()
 
     def sync_files(self, file_list):
         for file in file_list:
             self.sync_file(file)
 
     def stop_transfers(self):
-        pass
+        with self.transfers_lock:
+            for file, transfer in self.transfers.items():
+                transfer.shutdown()
+
+            self.transfers.clear()
 
 
 class FileTransfer(Thread):
-    def __init__(self):
+    def __init__(self, file_data):
         super().__init__()
+
+        self.file_data = file_data
+
+    def shutdown(self):
+        pass
+
+    def run(self):
+        # Transfer a file to the remote end
+        pass
+
+
+class FileTransferListener(Thread):
+    def __init__(self, remote):
+        super().__init__()
+
+        self.remote = remote
+
+    def run(self):
+        pass
