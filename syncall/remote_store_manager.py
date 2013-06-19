e35eb0f5d8811f6f65635e9c0ba5508b899cbd7a
Georgy Angelov <georgyangelov@gmail.com>
2013-06-19 20:44:17 +0300

    Fix some potential race conditions in RemoteStoreManager

--- syncall/remote_store_manager.py
+++ syncall/remote_store_manager.py
@@ -1,4 +1,5 @@
 import logging
+import threading
 
 import syncall
 
@@ -10,6 +11,7 @@ class RemoteStoreManager:
                  transfer_listener, directory, id):
         self.logger = logging.getLogger(__name__)
         self.remotes = dict()
+        self.remotes_lock = threading.Lock()
 
         self.directory = directory
         self.uuid = id
@@ -27,8 +29,9 @@ class RemoteStoreManager:
     def shutdown(self):
         # This will trigger their diconnected event and will remove them
         # from the remotes dictionary
-        for remote in self.remotes.values():
-            remote.disconnect()
+        with self.remotes_lock:
+            for remote in self.remotes.values():
+                remote.disconnect()
 
     def __transfer_initiated(self, transfer_messanger):
         # Accept the transfer connection and pass it to the
@@ -47,16 +50,18 @@ class RemoteStoreManager:
         self.remotes[remote_uuid].request_transfer(transfer_messanger)
 
     def __client_connected(self, messanger):
-        remote_ip = messanger.address[0]
-        remote_uuid = messanger.remote_uuid
+        with self.remotes_lock:
+            remote_ip = messanger.address[0]
+            remote_uuid = messanger.remote_uuid
 
-        if remote_uuid in self.remotes:
-            self.remotes[remote_uuid].disconnect()
+            if remote_uuid in self.remotes:
+                self.remotes[remote_uuid].disconnect()
 
-        remote_store = syncall.RemoteStore(messanger, self.directory)
-        remote_store.disconnected += self.__client_disconnected
+            remote_store = syncall.RemoteStore(messanger, self.directory)
+            remote_store.disconnected += self.__client_disconnected
 
-        self.remotes[remote_uuid] = remote_store
+            self.remotes[remote_uuid] = remote_store
+
         self.logger.info(
             "Remote connected from {}, UUID={}"
             .format(remote_ip, messanger.remote_uuid)
@@ -65,32 +70,33 @@ class RemoteStoreManager:
         remote_store.start_receiving()
 
     def __client_discovered(self, data):
-        remote_ip = data['source']
-        remote_uuid = data['data']['uuid']
+        with self.remotes_lock:
+            remote_ip = data['source']
+            remote_uuid = data['data']['uuid']
 
-        if remote_uuid not in self.remotes:
-            try:
-                messanger = syncall.Messanger.connect(
-                    (remote_ip, syncall.DEFAULT_PORT),
-                    self.uuid,
-                    remote_uuid
+            if remote_uuid not in self.remotes:
+                try:
+                    messanger = syncall.Messanger.connect(
+                        (remote_ip, syncall.DEFAULT_PORT),
+                        self.uuid,
+                        remote_uuid
+                    )
+                except Exception as ex:
+                    logger = logging.getLogger(__name__)
+                    logger.error("Couldn't connect to {}".format(address[0]))
+                    logger.exception(ex)
+
+                    return
+
+                remote_store = syncall.RemoteStore(messanger, self.directory)
+
+                self.remotes[remote_uuid] = remote_store
+                self.logger.info(
+                    "Connected to a remote at {}, UUID={}"
+                    .format(remote_ip, remote_uuid)
                 )
-            except Exception as ex:
-                logger = logging.getLogger(__name__)
-                logger.error("Couldn't connect to {}".format(address[0]))
-                logger.exception(ex)
 
-                return
-
-            remote_store = syncall.RemoteStore(messanger, self.directory)
-
-            self.remotes[remote_uuid] = remote_store
-            self.logger.info(
-                "Connected to a remote at {}, UUID={}"
-                .format(remote_ip, remote_uuid)
-            )
-
-            remote_store.start_receiving()
+                remote_store.start_receiving()
 
     def __client_disconnected(self, remote):
         self.logger.info(
